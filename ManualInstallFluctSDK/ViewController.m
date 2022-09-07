6f69ef649a81f9e39805900405439b326067f556
kugi <ramenjuniti@gmail.com>
2022-09-07 17:38:14 +0900

    update

--- ManualInstallFluctSDK/ViewController.m
+++ ManualInstallFluctSDK/ViewController.m
@@ -6,9 +6,9 @@
 //
 
 #import "ViewController.h"
+@import FluctSDK;
 
-@interface ViewController ()
-
+@interface ViewController () <FSSRewardedVideoDelegate>
 @end
 
 @implementation ViewController
@@ -16,7 +16,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    FSSRewardedVideo.sharedInstance.delegate = self;
+    
+    FSSRewardedVideoSetting *setting = FSSRewardedVideoSetting.defaultSetting;
+
+    setting.activation.fluctActivated = NO;
+    setting.debugMode = YES;
+
+    FSSRewardedVideo.sharedInstance.setting = setting;
+}
+- (IBAction)load:(id)sender {
+    [FSSRewardedVideo.sharedInstance loadRewardedVideoWithGroupId:@"1000083204" unitId:@"1000124351"];
 }
 
+- (IBAction)show:(id)sender {
+    if ([FSSRewardedVideo.sharedInstance hasAdAvailableForGroupId:@"1000083204" unitId:@"1000124351"]) {
+            [FSSRewardedVideo.sharedInstance presentRewardedVideoAdForGroupId:@"1000083204" unitId:@"1000124351" fromViewController:self];
+        }
+}
+
+- (void)rewardedVideoDidLoadForGroupID:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"rewarded video ad did load");
+}
+
+- (void)rewardedVideoShouldRewardForGroupID:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"should rewarded for app user");
+}
+
+- (void)rewardedVideoDidFailToLoadForGroupId:(NSString *)groupId unitId:(NSString *)unitId error:(NSError *)error {
+    // refs: error code list are FSSRewardedVideoError.h
+    NSLog(@"rewarded video ad load failed. Because %@", error);
+}
+
+- (void)rewardedVideoWillAppearForGroupId:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"rewarded video ad will appear");
+}
+
+- (void)rewardedVideoDidAppearForGroupId:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"rewarded video ad did appear");
+}
+
+- (void)rewardedVideoWillDisappearForGroupId:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"rewarded video ad will disappear");
+}
+
+- (void)rewardedVideoDidDisappearForGroupId:(NSString *)groupId unitId:(NSString *)unitId {
+    NSLog(@"rewarded video ad did disappear");
+}
+
+- (void)rewardedVideoDidFailToPlayForGroupId:(NSString *)groupId unitId:(NSString *)unitId error:(NSError *)error {
+    // refs: error code list are FSSRewardedVideoError.h
+    NSLog(@"rewarded video ad play failed. Because %@", error);
+}
 
 @end
---
+++ README.md
@@ -0,0 +1,27 @@
+# FluctSDK手動インストール
+
+1. https://github.com/voyagegroup/FluctSDK-iOS/releases から最新版の`Source code`をダウンロードする
+1. ダウンロードしたzipにある`FluctSDK.embeddedframework/FluctSDK.xcframework`をXcodeにドラッグアンドドロップし、`Build Phase`の`Link Binary With Libraries`に追加する
+1. ダウンロードしたzipにある`FluctSDK.embeddedframework/Resources/FluctSDKResources.bundle`をXcodeにドラッグアンドドロップし、`Build Phase`の`Copy Bundle Resources`に追加する
+1. ダウンロードしたzipにある`FluctSDK-MediationAdapter`をXcodeにドラッグアンドドロップして追加する
+1. 以下をframeworkをプロジェクトに追加する
+    - AdSupport
+    - CoreTelephony
+    - MediaPlayer
+    - CoreMedia
+    - SystemConfiguration
+    - StoreKit
+    - Social
+    - AVFoundation
+    - WebKit
+    - AppTrackingTransparency
+1. メディエーションで利用するアドネットワークSDKをインストールする
+    - [UnityAds](https://docs.unity.com/ads/InstallingTheiOSSDK.html)
+    - [Tapjoy](https://dev.tapjoy.com/jp/ios-sdk/Manual-Integration)
+    - [AppLovin](https://dash.applovin.com/documentation/mediation/manual-integration-ios)
+    - [AdColony](https://github.com/AdColony/AdColony-iOS-SDK/wiki/Project-Setup#option-2-non-cocoapods)
+    - [Pangle](https://www.pangleglobal.com/jp/integration/integrate-pangle-sdk-for-ios)
+    - [Maio](https://github.com/imobile-maio/maio-iOS-SDK#manual-download)
+    - [Nend](https://github.com/fan-ADN/nendSDK-iOS/wiki/SDK%E3%82%92%E6%89%8B%E5%8B%95%E3%81%A7%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8B)
+    - [Five](https://adsnetwork-docs.linebiz.com/fivesdk-ios/quick-start/install-fivesdk.html#%E6%89%8B%E5%8B%95%E3%81%A6%E3%82%99%E3%81%AE%E5%B0%8E%E5%85%A5)
+1. 動画リワード広告の表示確認をする
